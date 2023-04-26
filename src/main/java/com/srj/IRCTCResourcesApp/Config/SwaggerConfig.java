93ecebe079939735be1130a00c97a98ee89b0275
SurajSVikhe <vikhesuraj52@gmail.com>
2023-04-26 09:31:59 +0530

    hsgdkhs

--- src/main/java/com/srj/IRCTCResourcesApp/Config/SwaggerConfig.java
+++ src/main/java/com/srj/IRCTCResourcesApp/Config/SwaggerConfig.java
@@ -16,6 +16,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
 	
+//	@Bean
+//	public Docket apiDoc() {
+//		return new Docket(DocumentationType.SWAGGER_2)
+//			.select()
+//			.apis(RequestHandlerSelectors.basePackage("com.srj.IRCTCResourcesApp"))
+//			.paths(PathSelectors.any())
+//			.build();
+//		
+//	}
+
+	
 	@Bean
 	public Docket apiDoc() {
 		return new Docket(DocumentationType.SWAGGER_2)
